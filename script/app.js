3273bfbd95553d5484a5286d3a861b28b0dd3053
ElvisAmonde <ogollanyanja@gmail.com>
2025-02-04 22:35:32 +0300

    edited html

---
+++ script/app.js
@@ -0,0 +1,41 @@
+//utility functions for DOM  manipulation
+function createAnElement(element){
+    return document.createElement(element)    
+}
+
+function addText() {
+    return(element.innertext = text);
+}
+
+function appendChild(child, parent) {
+    return parent.appendChild(child);
+}
+function select(selector) {
+    return document.querySelector(selector);
+}
+
+function listen(params) {
+    
+}
+
+const h1 
+
+function addText () {
+    return h1.innertext = 56;
+}
+
+function appendchild (){
+    
+}
+
+function listen (params) {
+
+
+
+}
+
+// end of utility functions
+
+function appendChild () {
+   
+}
